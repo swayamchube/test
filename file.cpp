ff8dcd963e46857a69f16fcfe83967ccdd150a4c
Swayam Chube <yourbiologicalfather314@gmail.com>
2021-07-19 23:41:48 +0530

    sdlfjsdlf

--- file.cpp
+++ file.cpp
@@ -1,4 +1,6 @@
 #include <iostream>
 
 int main () {
+	std::cout << "Hello, World" << std::endl;
+	// You really shouldn't force flush the output but in this case, it is fine and will not cause much difference to the runtime.
 }
