3717cc417bb2a132961f49db06fb78607320da1f
Swayam Chube <yourbiologicalfather314@gmail.com>
2021-07-19 23:40:41 +0530

    removed redundant stuff

--- file.cpp
+++ file.cpp
@@ -1,12 +1,4 @@
-#include <bits/stdc++.h>
-using namespace std;
-
-typedef long long ll;
-typedef vector<int> vi;
-typedef set<int> si;
-
-#define pb push_back
-#define mp make_pair
+#include <iostream>
 
 int main () {
 }
